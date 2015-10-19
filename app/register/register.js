c5c0fa5d995b7d04dcd45a81bc5252c99d5d65d0
Kenneth Liu <ksliu25@gmail.com>
2015-10-19 18:58:24 -0500

    Add register controller and refactor view and controller

--- app/register/register.js
+++ app/register/register.js
@@ -1,7 +1,29 @@
 (function(){
 	'use strict';
 
-	angular.module('myApp.register', ['ngRoute'])
+	angular
+		.module('myApp.register', ['ngRoute'])
+		.controller('RegisterCtrl', RegisterCtrl)
 
-	.controller('RegisterCtrl', RegisterCtrl)
+		RegisterCtrl.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+		function RegisterCtrl(UserService, $location, $rootScope, FlashService){
+			var form = this;
+
+			form.register = register;
+
+			function register(){
+				form.dataLoading = true;
+				UserService.create(form.user)
+					.then(function(response){
+						if (response.success) {
+							FlashService.Success('Registration Successful!', true);
+							$location.path('/login');
+						} else {
+							FlashService.Error(response.message);
+							form.dataLoading = false;
+						}
+					});
+			}
+		}
+
 })();
