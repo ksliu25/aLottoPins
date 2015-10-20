59d2ad81aa41a240617902009468ffb1dc0267da
Kenneth Liu <ksliu25@gmail.com>
2015-10-20 10:44:39 -0500

    Refactor app.js styling and syntax

--- app/app.js
+++ app/app.js
@@ -1,25 +1,30 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngCookies',
-  'ngRoute',
-  'myApp.login',
-  'myApp.register',
-  'myApp.version'
-]).
-config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $routeProvider
-  .when('/login', {
-    templateUrl: 'login/login.html',
-    controller: 'LoginCtrl',
-    controllerAs: 'loginform'
-	})
-  .when('/register', {
-    templateUrl: 'register/register.html',
-    controller: 'RegisterCtrl',
-    controllerAs: 'registerform'
-  })
-  .otherwise({redirectTo: '/login'});
+angular
+  .module('myApp', [
+    'ngCookies',
+    'ngRoute',
+    'myApp.login',
+    'myApp.register',
+    'myApp.version'
+  ])
+  .config(config)
+  // .run(run);
 
-}])
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/login', {
+      templateUrl: 'login/login.html',
+      controller: 'LoginCtrl',
+      controllerAs: 'loginform'
+  	})
+    .when('/register', {
+      templateUrl: 'register/register.html',
+      controller: 'RegisterCtrl',
+      controllerAs: 'registerform'
+    })
+    .otherwise({redirectTo: '/login'});
+
+}
