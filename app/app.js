064e86175c50c62d1d53888a63a735e4d04a5768
Kenneth Liu <ksliu25@gmail.com>
2015-10-22 20:28:15 -0500

    install bower jquery, add route protection

--- app/app.js
+++ app/app.js
@@ -2,31 +2,50 @@
   'use strict';
 
 // Declare app level module which depends on views, and components
-angular
-  .module('myApp', [
-    'ngCookies',
-    'ngRoute',
-    'myApp.login',
-    'myApp.register',
-    'myApp.version'
-  ])
-  .config(config)
-  // .run(run);
+  angular
+    .module('myApp', [
+      'ngCookies',
+      'ngRoute',
+      'myApp.login',
+      'myApp.register',
+      'myApp.version'
+    ])
+    .config(config)
+    .run(run);
 
-config.$inject = ['$routeProvider', '$locationProvider'];
-function config($routeProvider, $locationProvider) {
-  $routeProvider
-    .when('/login', {
-      templateUrl: 'login/login.html',
-      controller: 'LoginCtrl',
-      controllerAs: 'loginform'
-  	})
-    .when('/register', {
-      templateUrl: 'register/register.html',
-      controller: 'RegisterCtrl',
-      controllerAs: 'registerform'
-    })
-    .otherwise({redirectTo: '/login'});
+  config.$inject = ['$routeProvider', '$locationProvider'];
+  function config($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/login', {
+        templateUrl: 'login/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'loginform'
+    	})
+      .when('/register', {
+        templateUrl: 'register/register.html',
+        controller: 'RegisterCtrl',
+        controllerAs: 'registerform'
+      })
+      .otherwise({redirectTo: '/login'});
 
-}
+  }
+
+  run.$inject = ['$rootScope', '$location', '$cookies', '$http'];
+  function run($rootScope, $location, $cookies, $http) {
+    // keep user logged in, thanks jason Watmore!!
+    $rootScope.globals = $cookies.get('globals') || {};
+    if ($rootScope.globals.currentUser) {
+      $http.defaults.headers.common['Authorization'] = 'Basic' + $rootScope.globals.currentUser.authdata;
+    }
+
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+      // redirect to login if not logged in and trying to acess restricted path
+      var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+      var loggedIn = $rootScope.globals.currentUser;
+      if (restrictedPage && !loggedIn) {
+        $location.path('/login');
+      }
+    });
+  }
+
 })();
