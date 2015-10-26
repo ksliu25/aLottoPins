165fc8044608504cdec6cbffb6a4d1034a4b0311
Kenneth Liu <ksliu25@gmail.com>
2015-10-26 10:13:23 -0500

    Add bowler service and callbacks

--- app/app_services/bowler.service.js
+++ app/app_services/bowler.service.js
@@ -17,9 +17,25 @@
 			return service;
 		}
 
-		function BowlerCreate(name){
-			$http.post
+		function BowlerCreate(name, callback){
+			$http.post('http://bowling-api.nextcapital.com/api/bowlers', {name: name})
+				.then(function successCallback(response){
+					callback(response);
+				});
 		}
 
+		function BowlersIndex(callback){
+			$http.get('http://bowling-api.nextcapital.com/api/bowlers')
+				.then(function successCallback(response){
+					callback(response);
+				});
+		}
+
+		function BowlersShow(bowlerId, callback){
+			$http.get('http://bowling-api.nextcapital.com/api/bowlers' + bowlerId)
+				.then(function successCallback(response){
+					callback(response);
+				});
+		}
 
 })();
