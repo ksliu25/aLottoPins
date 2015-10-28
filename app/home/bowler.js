434c89fb3269611f37a5fdcdd781f75afb575b20
Kenneth Liu <ksliu25@gmail.com>
2015-10-27 21:51:23 -0500

    abstract into bowlercontroller

---
+++ app/home/bowler.js
@@ -0,0 +1,45 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('myApp')
+		.controller('BowlerController', BowlerController);
+
+		BowlerController.$inject = ['BowlersService', 'FlashService', '$routeParams', '$location'];
+		function BowlerController(BowlersService, FlashService, $routeParams, $location){
+			var vm = this;
+			vm.bowlersAdd = bowlersAdd;
+			// vm.bowlerShow = bowlerShow;
+			vm.bowlers;
+			vm.bowlerId = $routeParams.bowlerId
+			bowlersIndex();
+			// console.log(bowlersIndex())
+
+			function bowlersAdd(name){
+				BowlersService.BowlerCreate(vm.name, function(response){
+					if (response.status === 200){
+						bowlersIndex();
+						FlashService.Success(vm.name + ' has been successfully created!', true);
+						$location.path('/');
+					}
+				})
+			}
+
+			function bowlersIndex(){
+				BowlersService.BowlersIndex(function(response){
+					vm.bowlers = response.data
+				});
+			}
+
+
+			// function bowlerShow(){
+
+			// }
+
+
+		}
+
+
+
+
+})();
--- app/home/home.js
+++ app/home/home.js
@@ -8,32 +8,7 @@
 		HomeController.$inject = ['BowlersService', 'FlashService', '$routeParams'];
 		function HomeController(BowlersService, FlashService, $routeParams){
 			var vm = this;
-			vm.bowlersAdd = bowlersAdd;
-			// vm.bowlerShow = bowlerShow;
-			vm.bowlers;
-			vm.bowlerId = $routeParams.bowlerId
-			bowlersIndex();
-			// console.log(bowlersIndex())
 
-			function bowlersAdd(name){
-				BowlersService.BowlerCreate(vm.name, function(response){
-					if (response.status === 200){
-						FlashService.Success(vm.name + ' has been successfully created!')
-						bowlersIndex();
-					}
-				})
-			}
-
-			function bowlersIndex(){
-				BowlersService.BowlersIndex(function(response){
-					vm.bowlers = response.data
-				});
-			}
-
-
-			// function bowlerShow(){
-
-			// }
 
 
 		}
