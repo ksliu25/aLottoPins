f54bd2d809ec48090b63a016290fc4e29794fc24
Kenneth Liu <ksliu25@gmail.com>
2015-10-29 19:15:10 -0500

    refactor lotteryshow page

--- app/home/lotterydetails.js
+++ app/home/lotterydetails.js
@@ -10,20 +10,13 @@
 			var vm = this;
 			vm.selectedLottery;
 			LotteryShow($stateParams.leagueId, $stateParams.lotteryId);
-			// vm.leagueLotteries;
-			// LeagueLotteries($stateParams.leagueId)
+
 
 			function LotteryShow(leagueId, lotteryId){
 				LotteriesService.LotteriesShow(leagueId, lotteryId, function(response){
 					vm.selectedLottery = response.data
 				});
 			}
-
-			// function LeagueLotteries(leagueId){
-			// 	LeaguesService.LeaguesBowlers(leagueId, function(response){
-			// 		vm.leagueLotteries = response.data
-			// 	});
-			// }
 
 		}
 
