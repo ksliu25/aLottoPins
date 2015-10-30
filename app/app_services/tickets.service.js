59eb5f8b5068ecbc59697abbd93aa08d5378fc7f
Kenneth Liu <ksliu25@gmail.com>
2015-10-29 20:21:50 -0500

    add ticketservice

--- app/app_services/tickets.service.js
+++ app/app_services/tickets.service.js
@@ -9,14 +9,41 @@
 		function TicketsService($http) {
 			var service = {};
 
-			service.TicketBuy = TicketBuy;
-			service.TicketLottery = TicketLottery;
-			service.TicketWinner = TicketWinner;
-			service.TicketRecord = TicketRecord;
-
-
+			service.TicketLotteryBuy = TicketLotteryBuy;
+			service.Tickets = Tickets;
+			service.TicketDrawWinner = TicketDrawWinner;
+			service.TicketRecordWinner = TicketRecordWinner;
 
 			return service;
+
+			function TicketLotteryBuy(leagueId, lotteryId, bowlerId, callback){
+				$http.post('/api/leagues/'+leagueId+'/lotteries/'+lotteryId+'/tickets', {bowler_id: bowlerId})
+					.then(function successCallback(response){
+						callback(response);
+					});
+			}
+
+			function Tickets(leagueId, lotteryId, callback){
+				$http.get('/api/leagues/'+leagueId+'/lotteries/'+lotteryId+'/tickets')
+					.then(function successCallback(response){
+						callback(response);
+					});
+			}
+
+			function TicketDrawWinner(leagueId, lotteryId, callback){
+				$http.get('/api/leagues/'+leagueId+'/lotteries/'+lotteryId+'/roll')
+					.then(function successCallback(response){
+						callback(response);
+					});
+			}
+
+			function TicketRecordWinner(leagueId, lotteryId, callback){
+				$http.get('/api/leagues/'+leagueId+'/lotteries/'+lotteryId+'/roll')
+					.then(function successCallback(response){
+						callback(response);
+					});
+			}
+
 		}
 
 
